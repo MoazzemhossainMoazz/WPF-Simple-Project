38694ed4584951533281bd36484b9ea58e1b24a0
unknown <moazzemh222@gmail.com>
2025-05-03 22:46:22 +0600

    Calculator interface

---
+++ Simple Project/Data Binding/Try_Catch_Throw/Try_Catch_Throw/Program.cs
@@ -0,0 +1,34 @@
+﻿class validInf
+{
+    public void vlidMsg(int age)
+    {
+        if (age < 18)
+        {
+            //Console.Write("Validation wrong: ");
+            
+            throw new Exception("You are not allowed apply for NID");
+        }
+        //Console.Write("Validation right: ");
+        //throw new Exception("Congratulations! ");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        validInf inf = new validInf();
+        inf.vlidMsg(5);
+
+        try
+        {
+            inf.vlidMsg(18);
+            Console.WriteLine("You are allowed to apply for NID");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Validation failed: {ex.Message}");
+        }
+
+    }
+}
