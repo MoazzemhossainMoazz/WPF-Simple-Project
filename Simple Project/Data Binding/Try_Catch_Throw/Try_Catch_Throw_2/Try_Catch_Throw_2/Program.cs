207525dddd7041034031bce256d863ba9762c8de
unknown <moazzemh222@gmail.com>
2025-05-05 23:28:37 +0600

    Try catch

---
+++ Simple Project/Data Binding/Try_Catch_Throw/Try_Catch_Throw_2/Try_Catch_Throw_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Headers;
+
+public class UserValidator
+{
+    public void ValidateAge(int age)
+    {
+        if(age < 18)
+        {
+            throw new Exception("You must have to be 18");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        UserValidator validinf = new UserValidator();
+        
+        try
+        {
+            validinf.ValidateAge(13);
+            Console.WriteLine("Congratulations! you are allowed to apply for NID");
+        }
+
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+}
